b81dec04d98866848175435e9d0cfe366d3393a5
GrayooKey <983034353@qq.com>
2020-01-04 17:15:09 +0800

    2

---
+++ src/test/java/com/graykey/xcore/testDemo/Demo1.java
@@ -0,0 +1,22 @@
+package com.graykey.xcore.testDemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试日志
+ *
+ * @author xuezb
+ * @date 2020-01-04
+ */
+@Slf4j
+public class Demo1 {
+
+
+    @Test
+    public void testSl4j() {
+//        logger.debug("sl4j.debug...");
+//        logger.info("sl4j.info");
+//        logger.error("sl4j.error");
+    }
+}
